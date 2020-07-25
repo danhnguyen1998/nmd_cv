b6ebe273f49b939c52c31a499a739e50b1f010d2
danh.nguyenminh <danh.nguyenminh@powergatesoftware.com>
2020-07-25 21:42:37 +0700

    init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,5 @@
 import React from "react";
 import "./App.css";
-import { Container, AppDiv } from "./styles";
-import About from "../components/organisms/About";
-import LoadingOverlay from "../components/organisms/LoadingOverlay";
-import Menu from "../components/organisms/Menu";
-import ThemeOption from "../components/organisms/ThemeOption";
-import ScrollTopBtn from "../components/organisms/ScrollTopBtn";
-import Skill from "../components/organisms/Skill";
-import Education from "../components/organisms/Education";
-import Experience from "../components/organisms/Experience";
-import Portfolio from "../components/organisms/Portfolio";
-import Testimonial from "../components/organisms/Testimonial";
-import Interest from "../components/organisms/Interest";
-import Blog from "../components/organisms/Blog";
-import Contact from "../components/organisms/Contact";
 
 const App: React.FC = () => {
     return (
