6c8e41006f22b0228ee2aa7fe12bfb410e06b3b0
danh.nguyenminh <danh.nguyenminh@powergatesoftware.com>
2020-07-26 03:46:31 +0700

    Updates

--- precache-manifest.35298feb1a911aefdd3ea1eb475c0957.js
+++ precache-manifest.35298feb1a911aefdd3ea1eb475c0957.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3077310c9894aa980169a620dbed6dd1",
+    "revision": "a64583dfd1ad8ecd5bf45402c4d4c5ac",
     "url": "/nmd_cv/index.html"
   },
   {
-    "revision": "7b1aabeed39526975a51",
+    "revision": "7c15ce86700ee44c01c0",
     "url": "/nmd_cv/static/css/main.d1b05096.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/nmd_cv/static/js/2.212cc32a.chunk.js.LICENSE"
   },
   {
-    "revision": "7b1aabeed39526975a51",
-    "url": "/nmd_cv/static/js/main.d5093c22.chunk.js"
+    "revision": "7c15ce86700ee44c01c0",
+    "url": "/nmd_cv/static/js/main.9b07c782.chunk.js"
   },
   {
     "revision": "ba02a344b83c9da0b3c2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nmd_cv/precache-manifest.d1eaa0fc3004b960e5dd91a6990f5b96.js"
+  "/nmd_cv/precache-manifest.35298feb1a911aefdd3ea1eb475c0957.js"
 );
 
 self.addEventListener('message', (event) => {
