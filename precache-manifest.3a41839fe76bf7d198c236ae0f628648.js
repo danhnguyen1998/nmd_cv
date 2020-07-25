5bcdadcd43cd721b592c4d095f0184a63c402c3d
danh.nguyenminh <danh.nguyenminh@powergatesoftware.com>
2020-07-25 21:14:12 +0700

    Updates

---
+++ precache-manifest.3a41839fe76bf7d198c236ae0f628648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877ac3a1e3b1d4793e2257e4057ec947",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "4ac03601373ea924f2ba",
+    "url": "/mycv/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "abacc9412a64ce4ccc31",
+    "url": "/mycv/static/js/2.74486af4.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/mycv/static/js/2.74486af4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ac03601373ea924f2ba",
+    "url": "/mycv/static/js/main.554ec249.chunk.js"
+  },
+  {
+    "revision": "4e6834a4b8817c89d3eb",
+    "url": "/mycv/static/js/runtime-main.2dfe24cc.js"
+  }
+]);
